0dca1d7f1bd69f9e3ccbf558b13edd7d01e0263b
Dev-webp <vjcbangalore@gmail.com>
2025-06-10 19:00:55 +0530

    first commit

---
+++ app/Popup/AustudImageContent.jsx
@@ -0,0 +1,22 @@
+// components/Popup/UkImageContent.jsx
+import React from "react";
+
+const UkImageContent = () => (
+  <div>
+      <h2 className="text-xl sm:text-3xl font-bold leading-tight bg-gradient-to-r from-orange-500 via-white to-sky-500 bg-clip-text text-transparent drop-shadow-lg flex items-center gap-2">
+        Study in Australia 2025
+      </h2>
+      <p className="mt-3 text-sm sm:text-base font-medium text-white leading-relaxed">
+        Study in {" "}
+        <span className="text-orange-400 font-semibold">
+          Australia and build a successful future
+        </span>{" "}
+        with world-class education, post-study work opportunities, and a pathway to permanent residency in this {" "}
+        <span className="text-orange-400 font-semibold">
+          vibrant multicultural society
+        </span>
+      </p>
+  </div>
+);
+
+export default UkImageContent;
